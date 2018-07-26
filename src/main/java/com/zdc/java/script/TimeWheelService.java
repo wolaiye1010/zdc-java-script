a0212543d2b45856205abecc74646ee1e119aaa5
zhudongchang <zhudongchang@58ganji.com>
2018-07-26 18:38:01 +0800

    时间轮  实现

---
+++ src/main/java/com/zdc/java/script/TimeWheelService.java
@@ -0,0 +1,3 @@
+package com.zdc.java.script;
+
+public class TimeWheelService {
---
+++ src/test/java/com/zdc/java/script/TimeWheelTest.java
@@ -0,0 +1,89 @@
+package com.zdc.java.script;
+
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zhudongchang on 2018/4/17.
+ */
+@Ignore
+public class TimeWheelTest {
+
+    @Before
+    public void before(){
+    }
+
+    @Test
+    public void timer() throws InterruptedException {
+        Timer time=new Timer();
+        System.out.println("      start:"+new Date());
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("task1 start:"+new Date());
+
+                throw new RuntimeException("aaa");
+//                try {
+//                    Thread.sleep(4000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        },1000);
+
+
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("task2 start:"+new Date());
+            }
+        },2000);
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+
+    @Test
+    public void ScheduledExecutorService() throws InterruptedException {
+        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(2);
+        Timer time=new Timer();
+        System.out.println("      start:"+new Date());
+
+        executorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("task1 start:"+new Date());
+                throw new RuntimeException("aaa");
+//                try {
+//                    Thread.sleep(4000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        },1000, TimeUnit.MILLISECONDS);
+
+        executorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("task2 start:"+new Date());
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },2000, TimeUnit.MILLISECONDS);
+
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
